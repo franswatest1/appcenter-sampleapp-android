e91da99f74f2dc91b10a59f8cd54cc8ea96f6fc8
franswatest1 <90319104+franswatest1@users.noreply.github.com>
2021-09-21 17:33:56 +0200

    update post build script

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 echo "This is an App Center Post-Build script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
+
+echo "Post Build Script Started"
+
+cat << EOF
+Check if maven is installed...
+check maven version
+EOF
+
+mvn --version
