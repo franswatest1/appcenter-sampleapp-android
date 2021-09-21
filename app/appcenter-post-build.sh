109e2ccd8ad95e4793a3999c066daea63f6a7b53
franswatest1 <90319104+franswatest1@users.noreply.github.com>
2021-09-21 19:06:15 +0200

    corrected appium ui test folder name

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -2,7 +2,7 @@
 
 echo "This is an App Center Post-Build script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
 
-AppiumFolder=`find "$APPCENTER_SOURCE_DIRECTORY" -name "appiumuitest" | head -1`
+AppiumFolder=`find "$APPCENTER_SOURCE_DIRECTORY" -name "appium-ui-test" | head -1`
 echo "Appium folder: $AppiumFolder"
 
 echo "Pack Appium test classes and all dependencies..."
