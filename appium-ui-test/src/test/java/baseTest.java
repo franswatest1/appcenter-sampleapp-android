5cc8d13f72062e01ac1f970cb2326f1e2d478215
franswatest1 <90319104+franswatest1@users.noreply.github.com>
2021-09-22 16:28:51 +0200

    Create baseTest.java

---
+++ appium-ui-test/src/test/java/baseTest.java
@@ -0,0 +1,61 @@
+import com.microsoft.appcenter.appium.EnhancedAndroidDriver;
+import com.microsoft.appcenter.appium.Factory;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+public class baseTest {
+  
+  @Rule
+  public TestWatcher watcher = Factory.createWatcher();
+  
+  private static EnhancedAndroidDriver<MobileElement> driver;
+  
+  public static EnhancedAndroidDriver<MobileElement> startApp() throws IOException {
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
+    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11");
+    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel3aTest");
+    capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 100);
+    capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+    capabilities.setCapability(MobileCapabilityType.APP_PACKAGE, "ms.appcenter.sampleapp.android");
+    capabilities.setCapability(MobileCapabilityType.APP_ACTIVITY, ".MainActivity");
+    
+    driver = Factory.createAndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    return driver;
+  }
+  
+  @Test
+  public void can StartAppInTest() throws IOException {
+    driver = startApp();
+    driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+    
+    // Verify homepage
+    MobileElement elem = driver.findElementByXPath("//android.widget.TextView[@text='Welcome']");
+    Assert.assertEquals(elem.getText(), "Welcome");
+    
+    // Click on build & verify build page
+    MobileElement build = driver.findElementByXPath("//android.widget.TextView[@text='Build']");
+    build.click();
+    
+    MobileElement buildtext = driver.findElementByAccessibilityId("ms.appcenter.sampleapp.android:id/buildTextView");
+    buildtext.click();
+    
+    Assert.assertTrue(buildtext.getText().contains("Create an installable app package automatically"));
+  }
+}
